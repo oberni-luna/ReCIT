cc83e20ae8e922d1ce677f72bc5ece2a694c4e60
Olivier Lunabee <olivier@lunabee.com>
2026-01-08 22:34:45 +0100

    Add inventory edition search flow

---
+++ ReCIT_iOS/Features/AppModels/Inventory/SearchResultsDTO.swift
@@ -0,0 +1,29 @@
+//
+//  SearchResultsDTO.swift
+//  ReCIT_iOS
+//
+//  Created by Olivier Berni on 12/12/2025.
+//
+
+import Foundation
+
+struct SearchResultsDTO: Codable {
+    let results: [SearchResultDTO]
+}
+
+struct SearchResultDTO: Codable {
+    let id: String
+    let type: String
+    let uri: String
+    let label: String
+    let description: String?
+    let image: String?
+}
+
+struct EditionSearchResult: Identifiable {
+    let id: String
+    let uri: String
+    let title: String
+    let description: String?
+    let imageUrl: String?
+}
